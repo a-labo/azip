a04b8bc5e915feda52eb12cac4f4e8c169e558e6
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-07 17:14:10 +0900

    Version incremented to 1.0.4

---
+++ test/zip_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for zip.
+ * Runs with mocha.
+ */
+'use strict'
+
+const zip = require('../lib/zip.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('zip', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Zip', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
